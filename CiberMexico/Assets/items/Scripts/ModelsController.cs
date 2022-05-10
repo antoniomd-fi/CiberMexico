ce2210b1480831adc545898b103c1ca286181525
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-05-10 13:05:43 -0500

    Feat: Model 1 and ModelController

---
+++ CiberMexico/Assets/items/Scripts/ModelsController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelsController : MonoBehaviour
+{
+    public GameObject modelo1;
+    //public GameObject modelo2;
+    public float radioDeAccion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GPSController.distancia < radioDeAccion)
+        {
+            modelo1.SetActive(true);
+            //modelo2.SetActive(false);
+        }
+        else
+        {
+            modelo1.SetActive(false);
+            //modelo2.SetActive(true);
+        }
+    }
+}
